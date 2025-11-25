d0e09505915210ae17496f97f44d1a8b3b1bab82
kalicg27 <102329198+kalicg27@users.noreply.github.com>
2025-11-25 17:26:55 +0100

    setting_changes

--- src/config/settings.py
+++ src/config/settings.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BASE_URL = os.getenv("BASE_URL", "https://reqres.in")
-REQUEST_TIMEOUT = int(os.getenv("REQUEST_TIMEOUT", "10"))
+BASE_URL = "https://reqres.in"
+REQUEST_TIMEOUT = 10
